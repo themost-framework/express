79f56026bc746f8921ec987fb1383cfe36253e79
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2025-09-26 19:37:08 +0300

    export ServiceConfiguration helper class (#77)
    * export ServiceConfiguration helper class
    
    * 2.4.1

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,5 @@
 // MOST Web Framework 2.0 Codename Blueshift Copyright (c) 2019-2023, THEMOST LP All rights reserved
+export * from './configuration';
 export * from './app';
 export * from './middleware';
 export * from './helpers';
